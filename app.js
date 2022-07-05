c96a3299ab7a4946034db3a3f83acbfd291b7821
teosilva72 <representante.com.sp@gmail.com>
2022-07-05 10:04:51 -0300

    one commit

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const conn = require('./setting/sql/mysql')
+const conn = require('./setting/sql/mysql');
+const ping = require('ping');
 const Conn = conn.Conn;
-setInterval(() => {
-    idGet()
-}, 20000)
 
-function idGet() {
-    let x = []
+setInterval(() => {
+    idGet();
+}, 60000)
+
+async function idGet() {
     Conn.query('SELECT equip_ip, name FROM sos_equipment', (e, result) => {
         adressEquip(JSON.parse(JSON.stringify(result)))
     });
@@ -14,11 +15,19 @@ function idGet() {
 function adressEquip(query) {
     for (var i = 0; i < query.length; i++) {
         if ((query[i].equip_ip && query[i].name) != '') {
-            console.log(query[i].equip_ip)
+            let ip = query[i].equip_ip
+            Ping(ip);
         }
     }
 }
 
-function ping() {
+async function Ping(ip) {
+
+    // WARNING: -i 2 argument may not work in other platform like windows
+    let res = await ping.promise.probe(ip, {
+        timeout: 10,
+        extra: ['-i', '2'],
+    });
+    console.log(res);
 
 }
